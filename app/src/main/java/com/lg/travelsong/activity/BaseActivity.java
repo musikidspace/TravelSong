8769f758cc2556fd7aefab0e40e03bbf48f4d67f
5201h <y5201h@hotmail.com>
2016-08-07 18:22:49 +0800

    自定义ActionBar主题

--- app/src/main/java/com/lg/travelsong/activity/BaseActivity.java
+++ app/src/main/java/com/lg/travelsong/activity/BaseActivity.java
@@ -7,14 +7,15 @@ import android.view.WindowManager;
 
 /**
  * Activity 基类
+ *
  * @author LuoYi on 2016/7/29
  */
-public class BaseActivity extends Activity{
+public class BaseActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // 透明状态栏
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         }
     }
--- app/src/main/java/com/lg/travelsong/activity/LoginActivity.java
+++ app/src/main/java/com/lg/travelsong/activity/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.lg.travelsong.activity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.lg.travelsong.R;
 
@@ -8,9 +11,24 @@ import com.lg.travelsong.R;
  * @author LuoYi on 2016/8/4
  */
 public class LoginActivity extends BaseActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initView();
+    }
+
+    private void initView() {
+        ActionBar ab = getActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_activity_main, menu);
+        return true;
     }
 }
